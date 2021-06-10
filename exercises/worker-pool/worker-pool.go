2397bf03aeab57222bbde5d9a9f985c757dc2601
Ravil Gabdullin <gvg_ravil@mail.ru>
2021-06-10 14:55:16 +0300

    style: strings per line

--- exercises/worker-pool/worker-pool.go
+++ exercises/worker-pool/worker-pool.go
@@ -11,7 +11,18 @@ import (
 	"time"
 )
 
-var images = []string{"https://www.hdwallpapers.in/download/brown_black_white_cat_kitten_is_walking_on_sand_4k_hd_kitten-HD.jpg", "https://www.hdwallpapers.in/download/colorful_digital_art_swirl_shapes_abstraction_4k_hd_abstract-HD.jpg", "https://www.hdwallpapers.in/download/yellow_eyes_brown_white_cat_in_blur_background_hd_cat-HD.jpg", "https://www.hdwallpapers.in/download/beach_4k_2-HD.jpg", "https://www.hdwallpapers.in/download/red_plant_leaves_with_water_drops_4k_hd_nature-HD.jpg", "https://www.hdwallpapers.in/download/snow_covered_landscape_mountains_valley_trees_forest_under_blue_sky_4k_hd_nature-HD.jpg", "https://www.hdwallpapers.in/download/snow_covered_mountains_forest_slope_trees_4k_hd_nature-HD.jpg", "https://www.hdwallpapers.in/download/snow_covered_mountains_peaks_fog_under_purple_black_cloudy_sky_4k_hd_nature-HD.jpg", "https://www.hdwallpapers.in/download/white_mountains_valley_trees_under_blue_sky_4k_hd_nature-HD.jpg", "https://www.hdwallpapers.in/download/blue_orange_fractal_shapes_circles_glare_hd_abstract-HD.jpg"}
+var images = []string{
+	"https://www.hdwallpapers.in/download/brown_black_white_cat_kitten_is_walking_on_sand_4k_hd_kitten-HD.jpg",
+	"https://www.hdwallpapers.in/download/colorful_digital_art_swirl_shapes_abstraction_4k_hd_abstract-HD.jpg",
+	"https://www.hdwallpapers.in/download/yellow_eyes_brown_white_cat_in_blur_background_hd_cat-HD.jpg",
+	"https://www.hdwallpapers.in/download/beach_4k_2-HD.jpg",
+	"https://www.hdwallpapers.in/download/red_plant_leaves_with_water_drops_4k_hd_nature-HD.jpg",
+	"https://www.hdwallpapers.in/download/snow_covered_landscape_mountains_valley_trees_forest_under_blue_sky_4k_hd_nature-HD.jpg",
+	"https://www.hdwallpapers.in/download/snow_covered_mountains_forest_slope_trees_4k_hd_nature-HD.jpg",
+	"https://www.hdwallpapers.in/download/snow_covered_mountains_peaks_fog_under_purple_black_cloudy_sky_4k_hd_nature-HD.jpg",
+	"https://www.hdwallpapers.in/download/white_mountains_valley_trees_under_blue_sky_4k_hd_nature-HD.jpg",
+	"https://www.hdwallpapers.in/download/blue_orange_fractal_shapes_circles_glare_hd_abstract-HD.jpg",
+}
 
 func downloadFile(URL, fileName string) error {
 	response, err := http.Get(URL)
@@ -43,7 +54,7 @@ func worker(jobs <-chan string, done chan<- struct{}) {
 		err := downloadFile(j, path.Base(j))
 		if err != nil {
 			fmt.Println(err)
-			fmt.Println("couldn't did the job, url: %s" + j)
+			fmt.Println("couldn't do the job, url: " + j)
 		}
 		done <- struct{}{}
 	}
@@ -60,7 +71,7 @@ func main() {
 	if len(os.Args) == 2 {
 		workersNum, _ = strconv.Atoi(os.Args[1])
 	}
-	if workersNum == 0 {
+	if workersNum <= 0 {
 		workersNum = 2
 	}
 
