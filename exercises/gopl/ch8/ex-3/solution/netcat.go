5137029f87ecc3b7e0be7c96f5b5f873ff6a0a55
Ravil Gabdullin <gvg_ravil@mail.ru>
2021-06-10 16:08:04 +0300

    feat: add solution of ex. 8.3

---
+++ exercises/gopl/ch8/ex-3/solution/netcat.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"os"
+	"time"
+)
+
+//!+
+func main() {
+	conn, err := net.Dial("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	done := make(chan struct{})
+	go func() {
+		io.Copy(os.Stdout, conn) // NOTE: ignoring errors
+		log.Println("done")
+		done <- struct{}{} // signal the main goroutine
+	}()
+
+	time.AfterFunc(5*time.Second, func() {
+		fmt.Println("writing is closed")
+		conn.(*net.TCPConn).CloseWrite()
+	})
+
+	mustCopy(conn, os.Stdin)
+
+	<-done // wait for background goroutine to finish
+}
+
+//!-
+
+func mustCopy(dst io.Writer, src io.Reader) {
+	if _, err := io.Copy(dst, src); err != nil {
+		log.Fatal(err)
+	}
+}
