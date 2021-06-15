85368f27c04720d4f4b7f60d022efc203c09f9d2
Ravil Gabdullin <gvg_ravil@mail.ru>
2021-06-15 14:11:04 +0300

    feat: acquaintance with the gin

---
+++ frameworks/gin/main.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"log"
+	"math/rand"
+	"net/http"
+	"os"
+	"os/signal"
+	"strconv"
+	"syscall"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Language uint
+
+const (
+	LangEn Language = iota
+	LangRu
+	LangFr
+	LangGe
+)
+
+type Book struct {
+	ID        int
+	Title     string    `json:"title"`
+	WrittenAt time.Time `json:"written_at"`
+	Language  Language  `json:"lang"`
+}
+
+var books = make(map[int]Book)
+
+var secrets = gin.H{
+	"foo":    gin.H{"email": "foo@bar.com", "phone": "123433"},
+	"austin": gin.H{"email": "austin@example.com", "phone": "666"},
+	"lena":   gin.H{"email": "lena@guapa.com", "phone": "523443"},
+}
+
+func init() {
+	rand.Seed(time.Now().Unix())
+}
+
+func main() {
+	router := gin.Default()
+
+	authorized := router.Group("/admin", gin.BasicAuth(gin.Accounts{
+		"foo":    "bar",
+		"austin": "1234",
+		"lena":   "hello2",
+		"manu":   "4321",
+	}))
+
+	authorized.GET("/secrets", func(c *gin.Context) {
+		user := c.MustGet(gin.AuthUserKey).(string)
+		if secret, ok := secrets[user]; ok {
+			c.JSON(http.StatusOK, gin.H{"user": user, "secret": secret})
+		} else {
+			c.JSON(http.StatusOK, gin.H{"user": user, "secret": "NO SECRET :("})
+		}
+	})
+
+	router.GET("/book/:id", func(c *gin.Context) {
+		idString := c.Param("id")
+		id, err := strconv.Atoi(idString)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "wrong id"})
+			return
+		}
+		book, ok := books[id]
+		if !ok {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "wrong id"})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{"book": book})
+	})
+
+	router.POST("/book", func(c *gin.Context) {
+		var book Book
+		if err := c.ShouldBindJSON(&book); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		book.ID = rand.Intn(100)
+		books[book.ID] = book
+		c.JSON(http.StatusCreated, gin.H{"id": book.ID})
+	})
+
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: router,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && errors.Is(err, http.ErrServerClosed) {
+			log.Printf("listen: %s\n", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server forced to shutdown:", err)
+	}
+
+	log.Println("Server exiting")
+}
